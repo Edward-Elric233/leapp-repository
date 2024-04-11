b0d9c5140a009792db322d803100e0250177d06e
edwardewang <edwardewang@tencent.com>
2024-04-11 16:45:50 +0800

    update rpm, add shell script

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ sed -i '$ a\fs.file-max = 16005535' /etc/sysctl.conf
 sysctl -p
 ulimit -n 102400
 dnf remove -y glibmm24-2.56.0-2.oc8.x86_64 crda-3.18_2020.04.29-1.oc8.noarch redhat-rpm-config-131-1.oc8.noarch
-leapp upgrade
+ls rpm/*.rpm | xargs dnf install -y
+nohub leapp upgrade&
